60e1f6d4c52002e1800ecc7d805348c9bc86930b
Anujkr-devOps <L00188535@atu.ie>
2025-11-20 22:39:25 +0000

    fix  PYTHONPATH for pytest

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,6 +1,8 @@
+import sys
+sys.path.append('app')
 from app import app
 def test_hello_world():
     client = app.test_client()
     response = client.get('/')
     assert response.status_code == 200
-    assert response.data == b'Hello, This is test response'  # Adjusted expected response for testing
+    assert response.data == b'Hello, World From DevOps AKS Pipeline! from AKS Pods for demo'
