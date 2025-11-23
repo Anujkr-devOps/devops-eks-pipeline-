c493f4b8c141906b5cad87ed6fc60ec0d69efb6b
Anujkr-devOps <L00188535@atu.ie>
2025-11-23 20:06:29 +0000

    fix demo.

--- tests/test_app.py
+++ tests/test_app.py
@@ -5,4 +5,4 @@ def test_hello_world():
     client = app.test_client()
     response = client.get('/')
     assert response.status_code == 200
-    assert response.data == b'Hello, World From DevOps AKS Pipeline! from AKS Pods for Video Demo'
+    assert response.data == b'Hello, World From DevOps AKS Pipeline! from AKS Pods for Demo'
